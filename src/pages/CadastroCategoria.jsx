524938bc4d5aa99c1825a57631eac589149c6d24
EstevaoSantos7 <since1178@gmail.com>
2025-08-14 15:39:56 -0300

    cadastro de categoria

--- src/pages/CadastroCategoria.jsx
+++ src/pages/CadastroCategoria.jsx
@@ -2,6 +2,11 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from '../components/Header'
+import pngCadastro from '../images/adicionarcategoria.png'
+import style from '../styles/cadastroCateg.style.module.css'
+import { FaRegTrashCan } from "react-icons/fa6";
+import { LuSave } from "react-icons/lu";
+
 
 const api = axios.create({
   baseURL: "http://localhost:3333"
@@ -33,36 +38,49 @@ export default function CadastroCategoria() {
   return (
     <>
       <Header />
-      <form action="" onSubmit={handleSubmit}>
-        <h2>Cadastro de categoria</h2>
-        <label htmlFor="nome">nome</label>
-        <input type="text"
-          id="nome"
-          placeholder="Digite um nome de categoria"
-          required
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />
-        <label htmlFor="descricao">Descrição</label>
-        <textarea
-          name="descricao"
-          id="descricao"
-          value={descricao}
-          placeholder="Digite uma descrição"
-          onChange={(e) => setDescricao(e.target.value)}
-          required
-        />
-        <div className="botoes">
-          <button type="button"
-            onClick={() => navigate(-1)}
-          >
-            Cancelar
-          </button>
-          <button type="submit" disabled={!isValid} >
-            Salvar
-          </button>
-        </div>
-      </form>
+      <div className={style.containerTudo}>
+      <div className={style.containerCadastro}>
+        <img src={pngCadastro} alt="" />
+
+        <form action="" onSubmit={handleSubmit}>
+          <h2>Cadastro de categoria</h2>
+          <div className={style.nomeDiv}>
+            <label className={style.label} htmlFor="nome">Nome</label>
+            <input type="text"
+              id="nome"
+              placeholder="Digite um nome de categoria"
+              required
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              className={style.nome}
+            />
+          </div>
+          <div className={style.descDiv}>
+            <label className={style.label} htmlFor="descricao">Descrição</label>
+            <textarea className={style.area}
+              name="descricao"
+              id="descricao"
+              value={descricao}
+              placeholder="Digite uma descrição"
+              onChange={(e) => setDescricao(e.target.value)}
+              required
+            />
+          </div>
+          <div className={style.botoes}>
+            <button type="button" className={style.botaoCancelar}
+              onClick={() => navigate(-1)}
+            >
+              Cancelar
+              <FaRegTrashCan color="white" />
+            </button>
+            <button type="submit" disabled={!isValid} className={style.botaoSalvar}>
+              Salvar
+              <LuSave color="white" />
+            </button>
+          </div>
+        </form>
+      </div>
+      </div>
     </>
   );
 }
