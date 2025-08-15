11379a0acaaaf4606ce762ada784948f822d7f19
EstevaoSantos7 <since1178@gmail.com>
2025-08-15 15:55:31 -0300

    fazendo produtos

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import Header from '../components/Header';
 import axios from 'axios';
+import style from '../styles/produtos.module.css';
 
 export default function Produtos() {
   const [produtos, setProdutos] = useState([]);
@@ -36,30 +37,36 @@ export default function Produtos() {
         value={busca}
         onChange={(e) => setBusca(e.target.value)}
       />
+      <div>
+        {produtosFiltrados.length === 0 ? (
+          <p>Nenhum produto foi cadastrado.</p>
+        ) : (
+          produtosFiltrados.map(produto => (
+            <div key={produto.id}>
+              <img
+                src={`http://localhost:3333${produto.imageUrl}`}
+                alt={produto.name}
+                width={100}
+              />
+              <p>ID: {produto.id}</p>
+              <p>Nome: {produto.name}</p>
+              <p>Categoria: {produto.category?.name || 'Sem categoria'}</p>
+              <p>Descrição: {produto.description}</p>
+              <div className={style.infoPreco}>
+                <p>Preço: R$ {produto.price.toFixed(2)}</p>
+                <p>Quantidade: {produto.quantity}</p>
+              </div>
 
-      {produtosFiltrados.length === 0 ? (
-        <p>Nenhum produto foi cadastrado.</p>
-      ) : (
-        produtosFiltrados.map(produto => (
-          <div key={produto.id}>
-            <p>ID: {produto.id}</p>
-            <p>Nome: {produto.name}</p>
-            <p>Descrição: {produto.description}</p>
-            <p>Preço: R$ {produto.price.toFixed(2)}</p>
-            <p>Quantidade: {produto.quantity}</p>
-            <p>Categoria: {produto.category?.name || 'Sem categoria'}</p>
-            <img
-              src={`http://localhost:3333${produto.imageUrl}`}
-              alt={produto.name}
-              width={100}
-            />
 
-            <Link to={`/editar-produto/${produto.id}`}>
-              Editar
-            </Link>
-          </div>
-        ))
-      )}
+
+              <Link to={`/editar-produto/${produto.id}`}>
+                Editar
+              </Link>
+            </div>
+          ))
+        )}
+      </div>
+
     </>
   );
 }
