63c4d5e265fc9f6860fb7198c9ec81dbc635b76c
EstevaoSantos7 <since1178@gmail>
2025-08-21 01:42:35 -0300

    Produtos caixas finalizado

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Header from '../components/Header';
 import axios from 'axios';
 import style from '../styles/produtos.module.css';
+import { FaPlus } from 'react-icons/fa';
+import Rodape from '../components/Rodape'
 
 export default function Produtos() {
   const [produtos, setProdutos] = useState([]);
@@ -25,48 +27,59 @@ export default function Produtos() {
     : produtos;
 
   return (
-    <>
+    <div className={style.containerTudo}>
       <Header />
-      <Link to="/cadastrar-produto">
+      <div className={style.caixaTop}>
+        <h1 className={style.tituloPage}>Produtos</h1>
+      <Link className={style.botaoAddProduto} to="/cadastrar-produto">
         Adicionar um produto
+        <FaPlus />
       </Link>
+      </div>
 
-      <input
+      {/* <input
         type="text"
         placeholder='Buscar produto'
         value={busca}
         onChange={(e) => setBusca(e.target.value)}
-      />
-      <div>
-        {produtosFiltrados.length === 0 ? (
-          <p>Nenhum produto foi cadastrado.</p>
-        ) : (
-          produtosFiltrados.map(produto => (
-            <div key={produto.id}>
-              <img
-                src={`http://localhost:3333${produto.imageUrl}`}
-                alt={produto.name}
-                width={100}
-              />
-              <p>ID: {produto.id}</p>
-              <p>Nome: {produto.name}</p>
-              <p>Categoria: {produto.category?.name || 'Sem categoria'}</p>
-              <p>Descrição: {produto.description}</p>
-              <div className={style.infoPreco}>
-                <p>Preço: R$ {produto.price.toFixed(2)}</p>
-                <p>Quantidade: {produto.quantity}</p>
+      /> */}
+
+
+      <div className={style.containerProdutos}>
+        <div className={style.containerProdutosGrid}>
+          {produtosFiltrados.length === 0 ? (
+            <p>Nenhum produto foi cadastrado.</p>
+          ) : (
+            produtosFiltrados.map(produto => (
+              <div className={style.produto} key={produto.id}>
+                <img
+                  src={`http://localhost:3333${produto.imageUrl}`}
+                  alt={produto.name}
+                  width={100}
+                />
+                <div className={style.infoProduto}>
+                  {/* <p>ID: {produto.id}</p> */}
+                  <p className={style.nomeProduto}> {produto.name}</p>
+                  <p className={style.categProduto}> {produto.category?.name || 'Sem categoria'}</p>
+                  <p className={style.descProduto}> {produto.description}</p>
+                  <div className={style.infoPreco}>
+                    <p className={style.preco}> R$ {produto.price.toFixed(2)}</p>
+                    <p className={style.quantidadeProduto}><span>Disponiveis:</span> {produto.quantity}</p>
+                  </div>
+                </div>
+
+
+
+                <Link className={style.editar} to={`/editar-produto/${produto.id}`}>
+                  Editar
+                </Link>
               </div>
-
-
-
-              <Link to={`/editar-produto/${produto.id}`}>
-                Editar
-              </Link>
-            </div>
-          ))
-        )}
+            ))
+          )}
+        </div>
       </div>
 
-    </>
+<Rodape/>
+    </div>
   );
 }
